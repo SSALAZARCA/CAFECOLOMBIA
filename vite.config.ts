888ec65e95832abd2069bd505a3921ba56954e9a
SSALAZARCA <ssalazarca@gmail.com>
2025-12-18 19:19:07 -0500

    fix(dev): update vite proxy to port 5001

--- vite.config.ts
+++ vite.config.ts
@@ -50,7 +50,7 @@ export default defineConfig(({ command, mode }) => {
       port: 5173,
       proxy: {
         '/api': {
-          target: env.VITE_API_BASE_URL || 'http://127.0.0.1:3001',
+          target: env.VITE_API_BASE_URL || 'http://127.0.0.1:5001',
           changeOrigin: true,
           secure: false,
         }
